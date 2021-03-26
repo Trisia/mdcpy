f3dc181eb371756627359c611b1a5ea4cc89c6cf
Quan guanyu <quanguanyu@qq.com>
2021-03-26 11:33:09 +0800

    关闭了提示日志

--- extension.js
+++ extension.js
@@ -15,7 +15,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "mdcpy" is now active!');
+	// console.log('Congratulations, your extension "mdcpy" is now active!');
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with  registerCommand
@@ -24,7 +24,7 @@ function activate(context) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Paste Processing!');
+		// vscode.window.showInformationMessage('Paste Processing!');
 
 		let editor = vscode.window.activeTextEditor;
 		let fileUri = editor.document.uri;
